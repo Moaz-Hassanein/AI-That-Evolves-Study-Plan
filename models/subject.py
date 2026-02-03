fbc4deb413fd18de089a3fc2188188ee995a95b2
OmarAyman <omaraymanzaid1122@gmail.com>
2026-02-03 14:46:41 +0200

    Add class subject

---
+++ models/subject.py
@@ -0,0 +1,7 @@
+class subject:
+    def __init__(self, name, deadline, tasks, difficulty_level, type):
+        self.name = name
+        self.deadline = deadline
+        self.tasks = tasks
+        self.difficulty_level = difficulty_level
+        self.type = type
